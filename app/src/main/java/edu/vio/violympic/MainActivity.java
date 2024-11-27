225e7c8e78474e1a9f4aa8a256e7ac4dcc890347
AnhPQ <anhpq@vtvlive.vn>
2024-11-27 17:49:29 +0700

    first commit

---
+++ app/src/main/java/edu/vio/violympic/MainActivity.java
@@ -0,0 +1,50 @@
+package edu.vio.violympic;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
+
+import java.util.ArrayList;
+
+import edu.vio.navigation.BubbleTabBar;
+import edu.vio.navigation.parser.BubbleMenuItem;
+import edu.vio.violympic.base.BaseViewModel;
+
+public class MainActivity extends AppCompatActivity {
+
+   private BaseViewModel viewModel;
+    private BubbleTabBar bottomNavMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        viewModel = new ViewModelProvider(this).get(BaseViewModel.class);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        bottomNavMain = findViewById(R.id.bottomNavMain);
+
+        ArrayList<BubbleMenuItem> menus = new ArrayList<>();
+        menus.add(new BubbleMenuItem(menus.size(), "Trang chủ", R.drawable.baseline_home_24, true, true));
+        menus.add(new BubbleMenuItem(menus.size(), "Trang chủ", R.drawable.baseline_home_24, true, false));
+        menus.add(new BubbleMenuItem(menus.size(), "Trang chủ", R.drawable.baseline_home_24, true, false));
+        menus.add(new BubbleMenuItem(menus.size(), "Tài khoản", edu.vio.navigation.R.drawable.ic_account, true, false));
+        bottomNavMain.setMenus(menus);
+
+        bottomNavMain.setOnBubbleReselectedListener(id -> {
+
+        });
+    }
+}
---
+++ app/src/main/java/edu/vio/violympic/base/BaseViewModel.java
@@ -0,0 +1,6 @@
+package edu.vio.violympic.base;
+
+import androidx.lifecycle.ViewModel;
+
+public class BaseViewModel extends ViewModel {
+}
